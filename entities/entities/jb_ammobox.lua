ed526e93e939b0ef71b9534aed08ba8b7eb60647
Splerge <damnsplurge@gmail.com>
2020-01-30 20:30:25 +1000

    Revert "."
    This reverts commit 7836ce7b6c2c3ff8744789b265f3cb9255792733.

--- entities/entities/jb_ammobox.lua
+++ entities/entities/jb_ammobox.lua
@@ -29,37 +29,44 @@
 -- ##                                                                                ##
 -- ##                                                                                ##
 -- ####################################################################################
-AddCSLuaFile()
-ENT.Type = "anim"
-ENT.Base = "base_anim"
 
-function ENT:Initialize()
-    if SERVER then
-        self:SetModel("models/Items/item_item_crate.mdl")
-        self:SetUseType(SIMPLE_USE)
-        self:PhysicsInit(SOLID_VPHYSICS)
-        self:SetMoveType(MOVETYPE_VPHYSICS)
-        self:SetSolid(SOLID_VPHYSICS)
-        local phys = self:GetPhysicsObject()
+AddCSLuaFile();
 
-        if (IsValid(phys)) then
-            phys:Wake()
-        end
-    end
+ENT.Type             = "anim"
+ENT.Base             = "base_anim"
+
+function ENT:Initialize()    
+	if SERVER then
+		self:SetModel( "models/Items/item_item_crate.mdl" );
+
+		self:SetUseType(SIMPLE_USE);
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:SetMoveType(MOVETYPE_VPHYSICS);
+		self:SetSolid(SOLID_VPHYSICS);
+		
+		local phys = self:GetPhysicsObject()
+		if ( IsValid( phys ) ) then
+			phys:Wake()	
+		end
+   
+	end
 end
 
 function ENT:Use(p)
-    if IsValid(p) and (not p.nextAmmoCrate or p.nextAmmoCrate < CurTime()) then
-        p.nextAmmoCrate = CurTime() + 5
-        p:GiveAmmo(128, "SMG1")
-        p:GiveAmmo(128, "Pistol")
-        p:SendNotification("You picked up an ammo crate")
-        self:Remove()
-    elseif IsValid(p) then
-        p:SendQuickNotification("You already picked up an ammo crate!")
-    end
+	if IsValid(p) and (not p.nextAmmoCrate or p.nextAmmoCrate < CurTime()) then
+		p.nextAmmoCrate = CurTime() + 5;
+	
+		p:GiveAmmo(128,"SMG1");
+		p:GiveAmmo(128,"Pistol");
+		
+		p:SendNotification("You picked up an ammo crate");
+		
+		self:Remove();
+	elseif IsValid(p) then
+		p:SendQuickNotification("You already picked up an ammo crate!");
+	end
 end
 
 function ENT:Draw()
-    self:DrawModel()
+	self:DrawModel();
 end
